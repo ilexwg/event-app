2dbfe02e3cac705f587a77014099f0c36c0078c7
ilexwg <ilexwg@gmail.com>
2021-09-03 13:31:02 +0800

    vuex part 1

--- src/store.js
+++ src/store.js
@@ -1,12 +1,49 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import EventService from '@/services/EventService.js';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community',
+    ],
+    events: [],
   },
-  mutations: {},
-  actions: {},
+
+  getters: {
+    catLength(state) {
+      return state.categories.length;
+    },
+    getEventById(state) {
+      return function(id) {
+        return state.events.find(event => event.id === id);
+      };
+    },
+  },
+
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event);
+    },
+    INCREMENT_COUNT(state, value) {
+      state.count += value;
+    },
+  },
+
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event);
+      });
+    },
+  },
 });
